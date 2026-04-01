a9b0dd497ba36c109078023ecc74642388f0c16d
Paramjeet singh <paramjeetsingh16346@gmail.com>
2026-04-01 22:01:35 +0530

    Time: 20 ms (66.19%) | Memory: 45.8 MB (70.22%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -14,9 +14,7 @@ class Solution {
                 int k = j+1;
                 int l = nums.length-1;
                 while(k<l){
-                    long sum = nums[i]+nums[j];
-                    sum+= nums[k];
-                    sum+= nums[l];
+                    long sum =(long) nums[i]+nums[j]+nums[k]+nums[l];
                     if(sum == target){
                       List<Integer> value = Arrays.asList(nums[i],nums[j],nums[k],nums[l]);
                       k++;
