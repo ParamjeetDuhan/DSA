d2c73e1f0b0ee32d3cf28c00ab90ff3624ee75b8
Paramjeet Singh <paramjeetsingh16346@gmail.com>
2026-07-07 08:39:37 +0530

    class name update

--- VS Code Practice/unique.java
+++ VS Code Practice/unique.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Unique{
+public class unique{
      public static void main(String[] args) {
         int[] arr = {1,5,7,3,2,4,7,2,2,5,6,8,90,5,3,2,1,1,3,46,7,8};
         Arrays.sort(arr);
